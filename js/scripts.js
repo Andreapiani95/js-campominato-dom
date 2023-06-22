4a6ac8597a36e425b01be342d42b3a43b8f42805
Andreapiani95 <andreapiani1995@gmail.com>
2023-06-22 12:42:59 +0200

    While cycle set

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
 document.getElementById('grid-generator').addEventListener('click',
     function (){
+        document.querySelector('.grid').innerHTML = "";
         createGrid(100);
     }
 );
 
-
-
-
 function createGrid(cellNumber){
     for (i = 1; i <= cellNumber; i++){
+        
         const gridCell = document.createElement('div');
         gridCell.append(i);
         gridCell.classList.add('flex-cell');
@@ -20,5 +19,27 @@ function createGrid(cellNumber){
     };
 }
 
+const bombArray = [];
+
+
+while (bombArray.length < 16){
+    const bombNumber = randomNumber (1, 100);
+    console.log(bombNumber);
+
+    if (!bombArray.includes(bombNumber)){
+        bombArray.push(bombNumber);
+    }
+
+}
+
+
+
+
+
+//Functions
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
 
     
