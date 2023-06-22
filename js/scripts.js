555c4a4eedfd6c5e25ad26cc6c588b277484f5ac
Andreapiani95 <andreapiani1995@gmail.com>
2023-06-22 16:04:16 +0200

    Complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
 document.getElementById('grid-generator').addEventListener('click',
     function (){
         document.querySelector('.grid').innerHTML = "";
+        document.getElementById('output').innerHTML = "";
         createGrid(100);
+        while (bombArray.length < 16){
+            const bombNumber = randomNumber (1, 100);
+            
+        
+            if (!bombArray.includes(bombNumber)){
+                bombArray.push(bombNumber);
+            }console.log(bombNumber);
+        }
     }
 );
+
+const selectedCell = []
+const bombArray = [];
 
 function createGrid(cellNumber){
     for (i = 1; i <= cellNumber; i++){
@@ -12,25 +24,25 @@ function createGrid(cellNumber){
         gridCell.append(i);
         gridCell.classList.add('flex-cell');
         document.querySelector('.grid').append(gridCell);
+
         gridCell.addEventListener('click', function(){
-            this.classList.toggle('selected')
-            console.log(this.innerHTML)
+            if (bombArray.includes(parseInt(this.innerHTML))){
+                this.classList.add('bomb');
+                gridCell.removeEventListener;
+                document.getElementById('output').innerHTML = `Peccato hai perso. Punteggio = ${selectedCell.length}`;
+            }
+            else {
+                this.classList.add('selected');
+                selectedCell.push(parseInt(this.innerHTML));
+                if (selectedCell.length > 83){
+                    gridCell.removeEventListener;
+                    document.getElementById('output').innerHTML = `Evviva, hai vinto! Punteggio = ${selectedCell.length}`;
+                }
+            };
         });
     };
 }
 
-const bombArray = [];
-
-
-while (bombArray.length < 16){
-    const bombNumber = randomNumber (1, 100);
-    console.log(bombNumber);
-
-    if (!bombArray.includes(bombNumber)){
-        bombArray.push(bombNumber);
-    }
-
-}
 
 
 
